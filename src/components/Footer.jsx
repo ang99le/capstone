99dc9a0b87c5ff28e3e367fd553518f5bf952456
unknown <malak.d993@gmail.com>
2024-05-24 02:12:52 +0300

    signup page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer(){
     const Navigate=useNavigate();
     return(
         <div>
-        <footer className=" grid grid-cols-2 px-5 text-white bg-sky-800 p-5">
-         <h6>CopyRights2024</h6>
-         <div className="hover:text-sky-600">
+        <footer className=" grid grid-cols-2 text-sm px-5 text-white bg-sky-800 p-2 lg:p-4">
+         <h6>Copyrights2024</h6>
+         <div className="hover:text-sky-600 text-sm">
          <a href="/Aboutus" onClick={()=>Navigate('/Aboutus')} >AboutUs</a>
          </div>
         </footer>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,11 +4,72 @@ function Signup() {
     return (
       <div className="App">
         <header>
-      SIGNUP
           <nav>
           <Navbar/>
           </nav>
         </header>
+       <main>
+       <main>
+<div class="flex min-h-full flex-col justify-center items-center w-[100%] h-[81vh] lg:h-[79vh] px-4  lg:px-8"  >
+
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
+    <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-sky-800">Create New Account</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="#" method="POST">
+
+    <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-sky-800">First Name</label>
+        <div class="mt-2">
+          <input id="text" name="text" type="text"  required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-800 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-sky-800">last Name</label>
+        <div class="mt-2">
+          <input id="text" name="text" type="text"  required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-800 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+
+
+
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-sky-800">Email address</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  focus:ring-sky-800 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-sky-800">Password</label>
+         
+        </div>
+        <div class="mt-2">
+        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  focus:ring-sky-800 sm:text-sm sm:leading-6" />
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-sky-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Sign in</button>
+      </div>
+      <div class="text-sm">
+            <a href="/Login" class="font-semibold text-sky-800 hover:text-sky-600">Already have an account</a>
+          </div>
+    </form>
+  </div>
+</div>
+</main>
+
+       </main>
+
+
+
+
         <footer>
           <Footer/>
         </footer>
