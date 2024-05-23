272767c725921dad9056c6179abe747e82095c21
unknown <malak.d993@gmail.com>
2024-05-24 01:51:59 +0300

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,12 +3,50 @@ import Footer from "../components/Footer";
 function Login() {
     return (
       <div className="App">
-      <header>
-      lOGIN
+     
       <nav>
       <Navbar/>
       </nav>
-      </header>
+     
+<main>
+<div class="flex min-h-full flex-col justify-center items-center w-[100%] h-[77vh] px-4  lg:px-8"  >
+
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
+    <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-sky-800">Log in to your account</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="#" method="POST">
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-sky-800">Email address</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-sky-800">Password</label>
+         
+        </div>
+        <div class="mt-2">
+        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-sky-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
+      <div class="text-sm">
+            <a href="#" class="font-semibold text-sky-800 hover:text-sky-600">Forgot password?</a>
+          </div>
+    </form>
+  </div>
+</div>
+</main>
+
+
+     
       <footer>
           <Footer/>
         </footer>
