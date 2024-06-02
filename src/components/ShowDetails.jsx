a1acc6424bff2153defc64594c5fea45a93038c5
unknown <malak.d993@gmail.com>
2024-06-02 22:43:59 +0300

    fix show details button

--- src/components/ShowDetails.jsx
+++ src/components/ShowDetails.jsx
@@ -5,18 +5,18 @@ function ShowDetails ({setPopUp}){
   const Navigate=useNavigate();
     return(
     <div className='w-screen h-screen bg-black bg-opacity-30 fixed top-0 right-0 flex justify-center items-center'>
-      <div className='bg-white p-10 rounded-md shadow-md'>
+      <div className='bg-white p-10 rounded-md shadow-md h-[650px]'>
 
       <div class="grid place-items-left">
       <button onClick={()=>setPopUp(false)}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF6eqIKPjVO3smnMpCieL6fiPOAbabJsgZgw&s" className="absloute  w-[20px]"></img></button>
       </div>
 
-        <h1 className=' text-sky-900 font-bold text-center text-lg my-5'>Rahma Riad Concert</h1>
-       <img className="w-[220px] rounded-xl  mx-auto mb-3" src="https://blog.dubaicityguide.com/site/wp-content/uploads/2022/12/Rahma-Riad.jpg"></img>
-        <p className="text-sky-800">
+        <h1 className=' text-sky-900 font-bold text-center text-lg my-4'>Rahma Riad Concert</h1>
+       <img className="w-[150px] rounded-xl  mx-auto mb-3" src="https://blog.dubaicityguide.com/site/wp-content/uploads/2022/12/Rahma-Riad.jpg"></img>
+        <p className="text-sky-800 text-sm">
         join Rahma Riad concert on Baghdad palm and live the <br/>beautiful vibe with one of the best female artist in the <br/>middle east don’t miss the opportunity and book your<br/> tickets now    
         </p>
-        <div className="flex mt-3 gap-3 justify-center items-center h-16">
+        <div className="flex mt-2 gap-3 justify-center items-center h-16 text-sm">
         <p className=" text-sky-800 font-medium leading-2 ">
          16th june
         </p>
