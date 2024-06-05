bfa450c79f0dc89a30cdf0fe4ab36c3aa0150681
unknown <malak.d993@gmail.com>
2024-06-05 19:58:58 +0300

    progdetails crouser

--- src/components/Progdetails.jsx
+++ src/components/Progdetails.jsx
@@ -9,83 +9,8 @@ function Progdetails(){
     </nav>
     <h1 className="text-center text-sky-800 font-medium text-lg mb-3 ">Babylon City Tour</h1>
 
-    <div class=" relative">
-         <div class="w-100 mx-auto">
-            {/* <!-- first --> */}
-            <div class="">
-               <input class="sr-only peer" type="radio" name="carousel" id="carousel-1" checked />
-               {/* <!-- content #1 --> */}
-               <div
-                  class=" absolute top-1/4 left-1/2 transform -translate-x-1/2 bg-white rounded-lg shadow-lg transition-all duration-300 opacity-0 peer-checked:opacity-100 peer-checked:z-10 z-0 md:w-[600px]">
-                  <img class="rounded-lg h-65 md:w-[600px]" src="https://listverse.com/wp-content/uploads/2012/11/Gate-of-Ishtar.jpg"/>
-                 
-                  {/* <!-- controls --> */}
-                  <div class="absolute top-1/2 w-full flex justify-between z-20">
-                     <label for="carousel-3" class="inline-block text-sky-800 cursor-pointer -translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
-                        </svg>
-                     </label>
-                     <label for="carousel-2" class="inline-block text-sky-800 cursor-pointer translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
-                        </svg>
-                     </label>
-                  </div>
-               </div>
-            </div>
-            {/* <!-- second --> */}
-            <div class="">
-               <input class="sr-only peer" type="radio" name="carousel" id="carousel-2" />
-               {/* <!-- content #2 --> */}
-               <div
-                  class="absolute top-1/4 left-1/2 transform -translate-x-1/2 bg-white rounded-lg shadow-lg transition-all duration-300 opacity-0 peer-checked:opacity-100 peer-checked:z-10 z-0 md:w-[600px]">
-                  <img class="rounded-lg h-65 md:w-[600px]" src="https://listverse.com/wp-content/uploads/2012/11/Gate-of-Ishtar.jpg"/>
-                 
-                  {/* <!-- controls --> */}
-                  <div class="absolute top-1/2 w-full flex justify-between z-20">
-                     <label for="carousel-3" class="inline-block text-sky-800 cursor-pointer -translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
-                        </svg>
-                     </label>
-                     <label for="carousel-2" class="inline-block text-sky-800 cursor-pointer translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
-                        </svg>
-                     </label>
-                  </div>
-               </div>
-            </div>
-            
-            {/* <!-- three --> */}
-            <div class="">
-               <input class="sr-only peer" type="radio" name="carousel" id="carousel-3" />
-               {/* <!-- content #3 --> */}
-               <div
-                  class=" absolute top-1/4 left-1/2 transform -translate-x-1/2 bg-white rounded-lg shadow-lg transition-all duration-300 opacity-0 peer-checked:opacity-100 peer-checked:z-10 z-0 md:w-[600px]">
-                  <img class="rounded-lg h-65 md:w-[600px]" src="https://listverse.com/wp-content/uploads/2012/11/Gate-of-Ishtar.jpg"  />
-                 
-                  {/* <!-- controls --> */}
-                  <div class="absolute top-1/2 w-full flex justify-between z-20">
-                     <label for="carousel-3" class="inline-block text-sky-800 cursor-pointer -translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
-                        </svg>
-                     </label>
-                     <label for="carousel-2" class="inline-block text-sky-800 cursor-pointer translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
-                        </svg>
-                     </label>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
 
-
-    <div class=" mt-40 p-6 lg:relative  text-sky-800 antialiased text-sm font-semibold md:mt-80 sm:flex justify-center">
+    <div class=" p-6 lg:relative  text-sky-800 antialiased text-sm font-semibold md:mt-80 sm:flex justify-center">
           
           <div class="p-6 relative mt-6 text-left ">
              
